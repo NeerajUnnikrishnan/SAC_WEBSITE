99b81736fe4e99827a8f4d9bace1f58e6a2097e1
Neeraj Unnikrishnan <neeraju14@gmail.com>
2015-10-02 18:41:00 -0700

    Mentorship and Imprint page Updated

--- imprint.php
+++ imprint.php
@@ -3,36 +3,57 @@
 <head> 
   <title>Students' Alumni Cell &middot; IIT Kharagpur</title>
    <?php include 'metatags.php'; ?>
-   <?php include 'css_scripts.php';?>  
+   <?php include 'css_include.php';?>  
 </head>
 <body >        
 	<div class="container">
-		
-		<?php include 'topheader.php'; ?>
+		<br>
+			<?php include 'topheader.php';?><br><br>
 		    <?php include 'navbar.php' ;?>
-	
-			<div >
-				<div  style="padding:10px;margin:10px;" >
+			<div class="row">
+				<div  class="col-sm-12"  >
+				    <img src="mi.jpg" style="width:100%;" >
 					<a href="http://www.webteam.iitkgp.ernet.in/scg/" style="text-decoration:none;"><h2>My Imprint</h2></a>
-					<h5>
-					IIT-KGP has a long and historic legacy of Alumni Giving Back which has existed in one or the other form.
-					The Graduating Class Gift is yet another chapter to be added in KGP alumni legacy initiated under My Imprint Program.
-					It is a class-based effort directed towards raising unrestricted fund partially used currently and partially going towards Endowment. 
-					This particular part of the Endowment would again be utilized for students' services only. It is an opportunity for graduating students 
-					to take their first step in being soon-to-be alumni by making their Graduating Class Gift, the gift which impacts student life in the KGP
-					campus. This challenge offers seniors the chance not only to make a gift, but also to leave behind their imprint
-					As the My Imprint Program says, "By the students, For the Students", the following 9 options have been formulated by My Imprint Class Gift
-					Committee, comprising of students only. Please click on The Committee tab to see the names of committee members.
-					The 9 options have been categorized in 3 slabs, according to the sign-ups raised by the Class of 2015 at the end of campaigning. 
-					You may make your choice of Class Gift in every slab. So, vote for 3 class gifts, one in each slab.<br><br><br><br><br><br><br><br><br>
-					<a href="http://www.webteam.iitkgp.ernet.in/scg/"  style="text-decoration:none;">Click Here</a> to visit My Imprint page
-					</h5>
+				    <p>The Student Alumni Mentorship programme was started in session 2010-11 by the Students’ Alumni Cell
+					as an initiative to create a platform for the exchange of ideas and experience between the alumni and 
+					the current students of IIT Kharagpur.
+					With an ocean of possibilities available, students often find it difficult to decide their way out
+					into the future. There are lots of instances where students need a mentor who can counsel and guide 
+					them through. The alumni can improve students' insight into various aspects of life. Their journey 
+					and personal experiences can help the students and motivate them through this pivotal phase of life.
+					Under this programme, we invite our alumni from across the world and from different fields to interact
+					with the students and help them build their network and gain new perspective on the possibilities open
+					to them after graduation.</p>
+					<br><br><hr><br>
+					 <h2>What is Senior Class Gift 2015 ?</h2>
+						<p>IIT-KGP has a long and historic legacy of Alumni Giving Back which has existed in one or the other form. The Graduating Class Gift is yet another chapter to be added in KGP alumni legacy initiated under My Imprint Program.
+						It is a class-based effort directed towards raising unrestricted fund partially used currently and partially going towards Endowment. This particular part of the Endowment would again be utilized for students' services only. It is an opportunity for graduating students to take their first step in being soon-to-be alumni by making their Graduating Class Gift, the gift which impacts student life in the KGP campus. This challenge offers seniors the chance not only to make a gift, but also to leave behind their imprint.</p>
+						<br><br><hr><br><br>
+                      <div class="row">
+					    <div class="col-sm-6" style="text-align:left; border-right: thick solid #ccc;">
+					  <form  method="post">
+					  <h3><span class="glyphicon glyphicon-user"></span>What do you think about Senior Class Gift?</h3>
+					  <br> 
+					  <input type="text" name="email" placeholder="Email" style="width:320px"/>
+					  <br>
+					  <br>
+					  <textarea name="comment" rows="5" cols="35" placeholder="Comment Here.." ></textarea>
+					  <br>					  
+					  <input type="submit" class="btn btn-info" value="submit"/>
+                      </form>
+					    </div>
+						<div class="col-sm-6"  style="text-align:right;"> 
+						<body data-spy="scroll" data-target=".navbar" data-offset="50">
+						"dolor efficiendi repudiandae at sed. Recteque principes quo id. Sed ut possit delectus. Unum legendos antiopam cu cum, recteque      moderatius adversarium no per. "-skdakjaja@gmail.com <br><br>
+						"Dolor meliore lucilius vim ut, mea cu quot delectus appellantur. Pro labores conclusionemque ut, pro audire fabellas in. At audire equidem ius, te mei repudiare consequuntur. Oblique iracundia honestatis mei eu, sea an habeo quaeque quaerendum, eu pri accusam  principes. "-jnjasa78@gmail.com <br><br>
+						"Sonet adipisci te cum, no pri graeci ceteros. Vis at vidisse veritus, ne nisl exerci verear qui, movet prodesset usu ne. Vim eu possit lucilius, cum ne debet iisque delectus. Fuisset voluptua gubergren nam no. Pro mutat facer no, nonumes maiorum at est."-goshje14@gmail.com <br><br>
+					    </div>					
+						
 			    </div>
-				
-			</div>
-			<?php include 'spons-slider.php'; ?>
+				<hr>
+		<?php include 'spons-slider.php'; ?>
 		<?php include 'footer.php';?>
-		<?php include 'script.php';?>
+		<?php include 'js_include.php';?>
 
 	</div>
 
--- mentor.php
+++ mentor.php
@@ -7,13 +7,16 @@
 </head>
 <body >        
 	<div class="container">
-			<?php include 'topheader.php';?>
+		<br>
+			<?php include 'topheader.php';?><br><br>
 		    <?php include 'navbar.php' ;?>
 	
-			<div >
-				<div  style="padding:10px;margin:10px;" >
-					<a href="http://www.webteam.iitkgp.ernet.in/scg/" style="text-decoration:none;"><h2>Mentorship Programme</h2></a>
-										<p>The Student Alumni Mentorship programme was started in session 2010-11 by the Students’ Alumni Cell
+			<div class="row">
+			    <br><br>
+				<div  class="col-sm-8" style="border-right:solid #ccc;"  >
+				        <img src="mp.jpg" style="width:100%;height:90px;border-radius:5px;" >
+				     <br><br>
+					 <p>The Student Alumni Mentorship programme was started in session 2010-11 by the Students’ Alumni Cell
 					as an initiative to create a platform for the exchange of ideas and experience between the alumni and 
 					the current students of IIT Kharagpur.
 					With an ocean of possibilities available, students often find it difficult to decide their way out
@@ -23,14 +26,65 @@
 					Under this programme, we invite our alumni from across the world and from different fields to interact
 					with the students and help them build their network and gain new perspective on the possibilities open
 					to them after graduation.
-				    For more information visit
-                   <a href="http://www.mentorship.iitkgp.ernet.in/mainpage/index" style="text-decoration:none;text-align:center;"> http://www.mentorship.iitkgp.ernet.in/mainpage/index</a>
-				   <br>
-					</p>
-			    </div>
-				
-			</div>
-		<?php include 'spons-slider.php'; ?>	
+					<br>  </div>
+					<div class="col-sm-4">					
+				   <br><br><br><br><br><br><br><br>
+				           <input type="submit" value="Register" class="btn btn-success btn-lg" >
+			       </div>
+				</div>
+				<div style="text-align:center;">
+				<h2 class="center"> Prominent Alumni Registered for this Programme </h2>
+				  <ul class="list-group" >
+					<li class="list-group-item prominent" id="1">Hardward Business School</li>
+					<li class="list-group-item prominent" id="4">Bank fo America</li>
+					<li class="list-group-item prominent" id="4">RBL</li>
+					<li class="list-group-item prominent" id="4">Citi Bank</li>
+					<li class="list-group-item prominent" id="10">McKinsey & Co</li>
+					<li class="list-group-item prominent" id="14">Credit Suisse</li>
+					<li class="list-group-item prominent" id="4">JP Morgan Chase</li>
+					<li class="list-group-item prominent" id="4">Standard Chartered Bank</li>
+					<li class="list-group-item prominent" id="4">Deutsche Bank</li>
+					<li class="list-group-item prominent" id="20">Hawkins Cookers Limited</li>
+					<li class="list-group-item prominent" id="10">Mindtree Limited</li>
+					<li class="list-group-item prominent" id="20">Lenovo</li>
+					<li class="list-group-item prominent" id="10">Delotti Consultancy SEA</li>
+					<li class="list-group-item prominent" id="18">Amazon</li>
+					<li class="list-group-item prominent" id="9">Hewelett Packard</li>
+					<li class="list-group-item prominent" id="10">Cognizant Technology</li>
+					<li class="list-group-item prominent" id="10">Infosys Limited</li>
+					<li class="list-group-item prominent" id="9">IBM</li>
+					<li class="list-group-item prominent" id="14">Stanford Gradute School of Business and Sustainable Growth Initiative</li>
+					<li class="list-group-item prominent" id="10">The Boston Consulting Group</li>
+					<li class="list-group-item prominent" id="18">Price Water House</li>
+					<li class="list-group-item prominent" id="14">PwC</li>
+					<li class="list-group-item prominent" id="10">Capgemini</li>
+					<li class="list-group-item prominent" id="10">Miebach Cousulting</li>
+					<li class="list-group-item prominent" id="14">KPMG</li>
+					<li class="list-group-item prominent" id="10">Schlumbreger</li>
+					<li class="list-group-item prominent" id="22">Sheil Oil Company</li>
+					<li class="list-group-item prominent" id="22">BP</li>
+					<li class="list-group-item prominent" id="22">Essar Oil Limited</li>
+					<li class="list-group-item prominent" id="15">Cairn India Limited</li>
+					<li class="list-group-item prominent" id="13">Reliance Industries</li>
+					<li class="list-group-item prominent" id="15">GE Oil & Gas</li>
+					<li class="list-group-item prominent" id="15">Occidental Petroleum</li>
+					<li class="list-group-item prominent" id="22">Indain Oil Corporation Limited</li>
+					<li class="list-group-item prominent" id="15">ONGC</li>
+					<li class="list-group-item prominent" id="all">MIT</li>
+					<li class="list-group-item prominent" id="all">Texas A & M</li>
+					<li class="list-group-item prominent" id="5">Wockhardt Ltd</li>
+					<li class="list-group-item prominent" id="9">Microsoft Research</li>
+					<li class="list-group-item prominent" id="10">Innovation Lab, Tata Consulantcy Services</li>
+					<li class="list-group-item prominent" id="all">Norwegian Unversity of Science and Technology</li>
+					<li class="list-group-item prominent" id="all">Georgia Institute of Technology</li>
+					<li class="list-group-item prominent" id="all">GFZ Potsdam</li>
+					<li class="list-group-item prominent" id="all">Swansea University</li>
+					<li class="list-group-item prominent" id="5">Dalichi Sankyo India Pharma Limited</li>
+					<li class="list-group-item prominent" id="all">University of Cincinnati</li>
+				</ul>
+                
+		
+		<?php include 'spons-slider.php'; ?>
 		<?php include 'footer.php';?>
 		<?php include 'js_include.php';?>
 
